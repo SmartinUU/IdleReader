4f4421795661ad04c9cefb657253970240571403
SmartinUU <1533204193@qq.com>
2018-06-15 11:43:54 +0800

    6.15

---
+++ app/src/main/java/cn/edu/gdmec/idlereader/fragment/NewsFragment.java
@@ -0,0 +1,23 @@
+package cn.edu.gdmec.idlereader.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cn.edu.gdmec.idlereader.R;
+
+/**
+ * Created by apple on 18/6/15.
+ */
+
+public class NewsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fg_news, container, false);
+    }
+}
